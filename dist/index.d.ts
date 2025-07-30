4d5fe7d93c42a9c0ab6d0f457184cde861c94a45
Xun Chen <xunchen369@gmail.com>
2025-07-30 19:51:29 +0800

    add dist folder

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+import type { Linter } from 'eslint';
+declare const configs: Linter.Config[];
+export default configs;
---
+++ dist/index.js
@@ -0,0 +1,46 @@
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+const reactConfig = {
+    files: ['**/*.{jsx,tsx}'],
+    plugins: {
+        react: reactPlugin,
+        'react-hooks': reactHooksPlugin,
+        'react-refresh': reactRefresh
+    },
+    languageOptions: {
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true
+            }
+        }
+    },
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    rules: {
+        'react/prop-types': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'off',
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true }
+        ]
+    }
+};
+const configs = [
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                React: 'readonly',
+                JSX: 'readonly'
+            }
+        }
+    },
+    reactConfig
+];
+export default configs;
